504b831d5703549f455ce845df1e6841b27f47c2
yuzoiwasaki <a0556017@sophia.jp>
2016-10-30 22:15:32 +0900

    cover all api test

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -6,19 +6,27 @@ describe 'GET /todos/:id' do
     @app = TodoApi
   end
 
-  before do
+  it '個別TODO情報を取得できる' do
     @todo = FactoryGirl.create(:todo)
     get "/todos/#{@todo.id}"
-  end
-
-  it '200 OK が返ってくる' do
-    expect(last_response.status).to eq 200
-  end
-
-  it 'TODO情報を取得できる' do
     json = JSON.parse(last_response.body)
     expect(json['title']).to eq(@todo.title)
     expect(json['description']).to eq(@todo.description)
     expect(json['status']).to eq(@todo.status)
   end
+
+  it '一覧TODO情報を取得できる' do
+    10.times { FactoryGirl.create(:todo) }
+    get "/todos"
+    json = JSON.parse(last_response.body)
+    expect(json.count).to eq 10
+  end
+
+  it 'TODOタスクのstatusを変更できる' do
+    @todo = FactoryGirl.create(:todo)
+    post "/todos/#{@todo.id}/status", '{"status": 1}'
+    updated_status = Todo.find_by(id: @todo.id).status
+    expect(updated_status).to eq 1
+  end
+
 end
